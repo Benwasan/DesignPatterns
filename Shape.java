2ef5e78c602438211c87090df767f4c2331114d8
Benwasan <46259955+Benwasan@users.noreply.github.com>
2020-10-21 13:55:44 +0200

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,10 @@
+package be.abis.execice.one;
+public abstract class Shape {
+    private String color;
+
+    public abstract double calculateArea();
+
+    public String getColor() {
+        return color;
+    }
+}
