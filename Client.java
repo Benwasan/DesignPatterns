c96533f45e09924a6d33f96798676e6c564548eb
Benwasan <46259955+Benwasan@users.noreply.github.com>
2020-10-21 14:10:56 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,48 +1,7 @@
-package DesignPatterns;
-
-abstract class Shape {
-	private String color;
-	
-	public abstract double calculateArea();
-
-	public String getColor() {
-		return color;
-	}
-}
-
-class Rectangle extends Shape {
-	private double height;
-	private double width;
-	private Point origin;
-	
-	public double calculateArea() {
-		return height * width;
-	}
-	
-	public double getHeight() {
-		return height;
-	}
-	
-	public double getWidth() {
-		return width;
-	}
-}
-
-class Circle extends Shape {
-	private double radius;
-	
-	public double calculateArea() {
-		return 3.14159265357989 *radius * radius;
-	}
-	
-	public double getRadius() {
-		return radius;
-	}	
-}
-
-class Point {	
-}
+package be.abis.hello.exercice1;
 
 public class Client {
-	private Shape shape;
+    public static void main(String[] args) {
+        Circle shape = new Circle();
+    }
 }
