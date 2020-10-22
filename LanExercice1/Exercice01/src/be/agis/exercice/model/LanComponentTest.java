0e9864f4424f649cead087e038ba6137848e0c8f
BENWASAN <benoit.corijn@sibecor.com>
2020-10-22 14:22:51 +0200

    First commit

--- LanExercice1/Exercice01/src/be/agis/exercice/model/LanComponentTest.java
+++ LanExercice1/Exercice01/src/be/agis/exercice/model/LanComponentTest.java
@@ -10,16 +10,18 @@ class LanComponentTest {
 
         PrintServer printServer1 = new PrintServer("192.168.0.100");
         PrintServer printServer2 = new PrintServer("192.168.0.101");
+        PrintServer printServer3 = new PrintServer("192.168.0.102");
 
         Node node1 = new Node("192.168.0.1");
         Node node2 = new Node("192.168.0.2");
 
-        workstation1.setNextComponent(node1);
+        workstation1.setNextComponent(printServer1);
+        printServer1.setNextComponent(node1);
         node1.setNextComponent(workstation2);
-        workstation2.setNextComponent(printServer1);
-        printServer1.setNextComponent(node2);
+        workstation2.setNextComponent(printServer2);
+        printServer2.setNextComponent(node2);
         node2.setNextComponent(workstation3);
-        workstation3.setNextComponent(printServer2);
+        workstation3.setNextComponent(printServer3);
 
         Packet packet1 = new Packet(printServer1.getAddress(), "MyMessage1");
         Packet packet2 = new Packet(printServer2.getAddress(), "MyMessage2");
--- LanExercice1/Exercice01/src/be/agis/exercice/model/Node.java
+++ LanExercice1/Exercice01/src/be/agis/exercice/model/Node.java
@@ -8,11 +8,20 @@ public class Node extends LanComponent {
 
     @Override
     public void send(Packet packet) {
-
+        packet.setDestinationAddress(this.address);
+        System.out.println("Packet sent to "+packet.getDestinationAddress());
+        receive(packet);
     }
 
     @Override
     public void receive(Packet packet) {
+        System.out.println("Packet "+packet.getContents()+" is received by "+this.getAddress());
 
+        if (packet.getDestinationAddress().equals(this.getAddress())) {
+            System.out.println("Packet received");
+        }
+        else {
+            this.getNextComponent().send(packet);
+        }
     }
 }
