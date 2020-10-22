787418ad754cd81ad77dbd21c3b077c891bc3273
BENWASAN <benoit.corijn@sibecor.com>
2020-10-22 13:26:04 +0200

    Version 1

---
+++ LanExercice1/Exercice01/src/be/agis/exercice/model/LanComponent.java
@@ -0,0 +1,27 @@
+package be.agis.exercice.model;
+
+public abstract class LanComponent {
+    String address="";
+    LanComponent nextComponent=null;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public LanComponent getNextComponent()
+    {
+        return nextComponent;
+    }
+
+    public void setNextComponent(LanComponent nextComponent) {
+        this.nextComponent = nextComponent;
+    }
+
+    public abstract void send(Packet packet);
+    public abstract void receive(Packet packet);
+}
---
+++ LanExercice1/Exercice01/src/be/agis/exercice/model/Node.java
@@ -0,0 +1,14 @@
+package be.agis.exercice.model;
+
+public class Node extends LanComponent {
+
+    @Override
+    public void send(Packet packet) {
+
+    }
+
+    @Override
+    public void receive(Packet packet) {
+
+    }
+}
