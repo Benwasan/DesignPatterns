872e7847d95037d9e53eeacccaf385ab59d85d1c
BENWASAN <benoit.corijn@sibecor.com>
2020-10-22 14:32:38 +0200

    First commit

--- LanExercice1/Exercice01/src/be/agis/exercice/model/LanComponentTest.java
+++ LanExercice1/Exercice01/src/be/agis/exercice/model/LanComponentTest.java
@@ -4,16 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LanComponentTest {
     public static void main(String[] args) {
-        WorkStation workstation1 = new WorkStation("192.168.0.230");
-        WorkStation workstation2 = new WorkStation("192.168.0.231");
-        WorkStation workstation3 = new WorkStation("192.168.0.232");
-
-        PrintServer printServer1 = new PrintServer("192.168.0.100");
-        PrintServer printServer2 = new PrintServer("192.168.0.101");
-        PrintServer printServer3 = new PrintServer("192.168.0.102");
-
-        Node node1 = new Node("192.168.0.1");
-        Node node2 = new Node("192.168.0.2");
+        WorkStation workstation1 = new WorkStation("192.168.0.1");
+        PrintServer printServer1 = new PrintServer("192.168.0.2");
+        Node node1 = new Node("192.168.0.3");
+        WorkStation workstation2 = new WorkStation("192.168.0.4");
+        PrintServer printServer2 = new PrintServer("192.168.0.5");
+        Node node2 = new Node("192.168.0.6");
+        WorkStation workstation3 = new WorkStation("192.168.0.7");
+        PrintServer printServer3 = new PrintServer("192.168.0.8");
 
         workstation1.setNextComponent(printServer1);
         printServer1.setNextComponent(node1);
@@ -25,8 +23,10 @@ class LanComponentTest {
 
         Packet packet1 = new Packet(printServer1.getAddress(), "MyMessage1");
         Packet packet2 = new Packet(printServer2.getAddress(), "MyMessage2");
+        Packet packet3 = new Packet(printServer3.getAddress(), "MyMessage3");
 
-        workstation1.originate(packet1);
-        workstation1.send(packet1);
+        workstation1.send(packet3);
+        workstation2.send(packet3);
+        workstation3.send(packet1);
     }
 }
