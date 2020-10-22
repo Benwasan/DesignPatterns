325384511f8c145dab956ea4a54a05381b105340
BENWASAN <benoit.corijn@sibecor.com>
2020-10-22 14:08:26 +0200

    First commit

--- LanExercice1/Exercice01/src/be/agis/exercice/model/LanComponentTest.java
+++ LanExercice1/Exercice01/src/be/agis/exercice/model/LanComponentTest.java
@@ -1,4 +1,30 @@
+package be.agis.exercice.model;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class LanComponentTest {
-  
+    public static void main(String[] args) {
+        WorkStation workstation1 = new WorkStation("192.168.0.230");
+        WorkStation workstation2 = new WorkStation("192.168.0.231");
+        WorkStation workstation3 = new WorkStation("192.168.0.232");
+
+        PrintServer printServer1 = new PrintServer("192.168.0.100");
+        PrintServer printServer2 = new PrintServer("192.168.0.101");
+
+        Node node1 = new Node("192.168.0.1");
+        Node node2 = new Node("192.168.0.2");
+
+        workstation1.setNextComponent(node1);
+        node1.setNextComponent(workstation2);
+        workstation2.setNextComponent(printServer1);
+        printServer1.setNextComponent(node2);
+        node2.setNextComponent(workstation3);
+        workstation3.setNextComponent(printServer2);
+
+        Packet packet1 = new Packet(printServer1.getAddress(), "MyMessage1");
+        Packet packet2 = new Packet(printServer2.getAddress(), "MyMessage2");
+
+        workstation1.originate(packet1);
+
+    }
 }
